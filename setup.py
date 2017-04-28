e4683ee755a410421d838f8dd37f350d26d80163
RoxanneYang <yanzheyangchn@outlook.com>
2017-04-28 16:01:47 +0800

    Add example and setup files

---
+++ setup.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import find_packages
+from setuptools import setup
+
+PACKAGE = 'WindAdapter'
+NAME = 'WindAdapter'
+VERSION = '0.0.1'
+DESCRIPTION = 'Windpy data adapter'
+AUTHOR = 'iLampard, RoxanneYang'
+URL = 'https://github.com/iLampard/WindAdapter'
+LICENSE = 'MIT'
+
+requirements = 'requirements.txt'
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      url=URL,
+      packages=find_packages(),
+      install_requires=[
+          'numpy>=1.10.1',
+          'pandas>=0.18.0',
+          'python-decouple>=3.0'
+      ],
+      classifiers=['Programming Language :: Python',
+                   'Programming Language :: Python :: 2.7',
+                   'Programming Language :: Python :: 3.5'])
