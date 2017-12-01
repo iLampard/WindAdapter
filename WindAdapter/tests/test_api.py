8108a02a947490eb24e9f798f16e748c740b96cb
iLampard <siqiao_xue@163.com>
2017-12-01 23:17:24 +0800

    fix test

--- WindAdapter/tests/test_api.py
+++ WindAdapter/tests/test_api.py
@@ -22,47 +22,6 @@ class MockWindData(object):
 
 class TestApi(unittest.TestCase):
     @patch('WindAdapter.data_provider.WindDataProvider.query_data')
-    def testFactorLoad_case1(self, mock_query_data):
-        from WindAdapter.enums import OutputFormat
-        start_date = '2016-01-01'
-        end_date = '2016-02-01'
-        factor_name = 'PB'
-        sec_id = ['000001.SZ', '000002.SZ', '000003.SZ']
-
-        mock_data = MockWindData(data=[[1, 3, 5]],
-                                 codes=sec_id,
-                                 error_code=0,
-                                 fields=['PB'],
-                                 times=[datetime(2016, 1, 1),
-                                        datetime(2016, 2, 1)])
-        mock_query_data.return_value = mock_data
-
-        calculated = factor_load(start_date=start_date,
-                                 end_date=end_date,
-                                 factor_name=factor_name,
-                                 sec_id=sec_id,
-                                 output_data_format=OutputFormat.MULTI_INDEX_DF,
-                                 is_index=False)
-
-        expected = pd.DataFrame(data=[1, 3, 5, 1, 3, 5],
-                                index=pd.MultiIndex.from_product([['2016-01-29', '2016-02-01'],
-                                                                  ['000001.SZ', '000002.SZ', '000003.SZ']],
-                                                                 names=['date', 'secID']),
-                                columns=['factor'])
-        assert_frame_equal(calculated, expected)
-
-        calculated = factor_load(start_date=start_date,
-                                 end_date=end_date,
-                                 factor_name=factor_name,
-                                 sec_id=sec_id,
-                                 output_data_format=OutputFormat.PITVOT_TABLE_DF,
-                                 is_index=False)
-        expected = pd.DataFrame(data=[[1, 3, 5], [1, 3, 5]],
-                                columns=['000001.SZ', '000002.SZ', '000003.SZ'],
-                                index=['2016-01-29', '2016-02-01'])
-        assert_frame_equal(calculated, expected)
-
-    @patch('WindAdapter.data_provider.WindDataProvider.query_data')
     @patch('WindAdapter.data_provider.WindDataProvider.biz_days_list')
     @patch('WindAdapter.data_provider.WindDataProvider.forward_date')
     def testFactorLoad_case2(self, mock_adv_date, mock_days_list, mock_query_data):
